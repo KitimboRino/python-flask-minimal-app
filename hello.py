90407bbc79e455d9c02cc9d3cbcd35c31a7b5722
Kitimbo Rino <kitimborinoemma0580@gmail.com>
2024-05-13 17:29:04 +0300

    feat(error pages): custom error pages

--- hello.py
+++ hello.py
@@ -4,11 +4,22 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+# Error handling 400
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
+# Error handling 500
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+# Home route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Usper route
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
